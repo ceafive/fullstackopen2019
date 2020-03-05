9c363c0a6b0e9ad74c93eb5af9b969ae630ea10d
ceafive <ceafive@gmail.com>
2020-03-05 16:22:16 +0000

    The Phonebook Step2

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -2,18 +2,27 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: 1 }
   ])
   const [newName, setNewName] = useState('')
 
   const addName = (e) => {
     e.preventDefault();
+    const found = persons.find(person => {
+      const personExists = person.name
+      return personExists.toUpperCase() === newName.toUpperCase()
+    })
+    console.log("Duplicate entry:", found)
+    if (found) {
+      return alert(`${newName} is already added to the phonebook`)
+    }
     const newEntry = {
-      name: newName
+      name: newName,
+      id: persons.length + 1
     }
     setPersons(persons.concat(newEntry))
   }
-  console.log(persons)
+
 
 
   return (
@@ -28,7 +37,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map(person => <li key={person.name}>{person.name}</li>)}</div>
+      <div>{persons.map(person => <p key={person.id}>{person.name}</p>)}</div>
       <div>debug: {newName}</div>
     </div>
   )
